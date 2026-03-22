a426678272ecd051192abe03208ceaef4ebf8392
jeongwooLee1 <150905300+jeongwooLee1@users.noreply.github.com>
2026-03-22 09:12:56 +0900

    fix: sw.js v2.69.404

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bliss-v2.69.402";
+const CACHE = "bliss-v2.69.404";
 const ASSETS = ["/bliss/", "/bliss/index.html"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
